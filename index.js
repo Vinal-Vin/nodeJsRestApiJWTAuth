1b6b497e86a09630592a2305f806bab5815a58ac
Vinal-Vin <kvinal20@gmail.com>
2021-12-31 10:01:32 +1200

    configured dotenv

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express();
+const dotenv = require('dotenv');
+const db = require('mongoose');
 
+dotenv.config();
 
 //Import Routes
 const authRoutes = require('./Routes/auth');
 
 // Route middlewares
-app.use('/api/user', authRoutes)
+app.use('/api/user', authRoutes);
+
+//MongoDB Connection
+db.connect(process.env.DB_CONNECT, () => {
+    console.log("Connected to DB!.......");
+});
 
 
 app.listen(3000, () => {console.log('Server is up & running')});
